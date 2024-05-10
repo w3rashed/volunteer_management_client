f5bf0029ffed02ec802adc2d854d423e5e620363
Rashed <w3rashed@gmail.com>
2024-05-10 12:04:04 +0600

    get all volunteer data

--- src/Components/NeedVolunteer/NeedVolunteer.jsx
+++ src/Components/NeedVolunteer/NeedVolunteer.jsx
@@ -1,22 +1,41 @@
-import axios from "axios";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../AuthProvider_&_Firebase/AuthProvider";
+import axios from "axios";
+import { BsGrid3X3Gap } from "react-icons/bs";
+import { CgLayoutList } from "react-icons/cg";
 
 const NeedVolunteer = () => {
   const { setLoading } = useContext(AuthContext);
-  axios
-    .get("http://localhost:5000/volunteer_post")
-    .then((res) => {
-      setLoading(true);
-      console.log(res.data);
-    })
-    .catch((error) => {
-      console.log(error);
-    });
+  const [lodedData, setLodedData] = useState([]);
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/volunteer_post")
+      .then((res) => {
+        //   console.log(res.data);
+        setLoading(false);
+        setLodedData(res.data);
+      })
+      .catch((error) => {
+        setLoading(true);
+        console.log(error);
+      });
+  }, []);
+  console.log(lodedData);
 
   return (
     <div>
-      <h2>NeedVolunteer</h2>
+      <h2>NeedVolunteer:{lodedData.length}</h2>
+      <div role="tablist" className="tabs">
+        <a role="tab" className="tab">
+          <BsGrid3X3Gap></BsGrid3X3Gap>
+          <div>
+            <h2>hiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii</h2>
+          </div>
+        </a>
+        <a role="tab" className="tab tab-active">
+          <CgLayoutList></CgLayoutList>
+        </a>
+      </div>
     </div>
   );
 };
--- src/Routes/PrivetRout/PrivetRoute.jsx
+++ src/Routes/PrivetRout/PrivetRoute.jsx
@@ -1,9 +1,22 @@
 import { useContext } from "react";
 import { AuthContext } from "../../AuthProvider_&_Firebase/AuthProvider";
+import PropTypes from "prop-types";
 
 const PrivetRoute = ({ children }) => {
   const { user, loading } = useContext(AuthContext);
-  return <div></div>;
+
+  if (loading) {
+    return (
+      <span className="loading loading-spinner text-info flex mx-auto  h-screen items-center"></span>
+    );
+  }
+  if (user) {
+    return children;
+  }
+ 
 };
 
+PrivetRoute.propTypes = {
+  children: PropTypes.node.isRequired,
+};
 export default PrivetRoute;
