ce0570f1215a7aed263e099e43f7acd519dceb6b
Rashed <w3rashed@gmail.com>
2024-05-10 16:01:33 +0600

    display all volunteer data

--- src/Components/NeedVolunteer/NeedVolunteer.jsx
+++ src/Components/NeedVolunteer/NeedVolunteer.jsx
@@ -3,6 +3,9 @@ import { AuthContext } from "../../AuthProvider_&_Firebase/AuthProvider";
 import axios from "axios";
 import { BsGrid3X3Gap } from "react-icons/bs";
 import { CgLayoutList } from "react-icons/cg";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
+import { Link } from "react-router-dom";
 
 const NeedVolunteer = () => {
   const { setLoading } = useContext(AuthContext);
@@ -22,19 +25,59 @@ const NeedVolunteer = () => {
   }, []);
   console.log(lodedData);
 
+  const {
+    thumbnail,
+    title,
+    description,
+    category,
+    location,
+    deadline,
+    name,
+    email,
+  } = lodedData;
+
   return (
     <div>
       <h2>NeedVolunteer:{lodedData.length}</h2>
-      <div role="tablist" className="tabs">
-        <a role="tab" className="tab">
-          <BsGrid3X3Gap></BsGrid3X3Gap>
-          <div>
-            <h2>hiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii</h2>
-          </div>
-        </a>
-        <a role="tab" className="tab tab-active">
-          <CgLayoutList></CgLayoutList>
-        </a>
+      <div>
+        <Tabs>
+          <TabList>
+            <Tab>
+              <BsGrid3X3Gap></BsGrid3X3Gap>
+            </Tab>
+            <Tab>
+              <CgLayoutList className="text-2xl"></CgLayoutList>
+            </Tab>
+          </TabList>
+          {/* layout card */}
+          <TabPanel>
+            <div className="grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+              {lodedData.map((data) => (
+                <div key={data._id} className="card  bg-base-100 shadow-xl">
+                  <figure>
+                    <img
+                      src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+                      alt="Shoes"
+                    />
+                  </figure>
+                  <div className="card-body">
+                    <h2 className="card-title">{data.title}</h2>
+                    <h2 className="card-title">{data.deadline}</h2>
+                    <p>{data.category}</p>
+                    <p>{data.location}</p>
+                    <Link to={`/view_details/${data._id}`}>
+                      <button className="btn">View Details</button>
+                    </Link>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </TabPanel>
+          {/* layout table */}
+          <TabPanel>
+            <h2>Any content 2</h2>
+          </TabPanel>
+        </Tabs>
       </div>
     </div>
   );
